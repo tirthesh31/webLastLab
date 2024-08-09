c747f1adfdbe0b200e46460c2b5ae9f25dd36952
tirthesh31 <tirtheshmotiwala@gmail.com>
2024-08-09 15:36:49 -0400

    first and final commit

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          'index.html',
+          'home.html',
+          'page2.html',
+          'page3.html',
+          'manifest.json'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
